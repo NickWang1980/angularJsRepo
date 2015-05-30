970c327fbae124548650ed017714e15877898fb7
NickWang1980 <NickWang1980@gmail.com>
2015-05-30 18:28:34 +0800

    updating

--- angularJsReadOracle/WebContent/app/app.js
+++ angularJsReadOracle/WebContent/app/app.js
@@ -2,9 +2,8 @@ app = angular.module('app', [ 'ngMaterial', 'ngMessages', 'ngRoute' ]);
 
 app.controller('oracleDbMonitorController', function($scope, $http, $log) {
 
-	$scope.mySqlStr="";
-	
-//	$log.info("$scope.mySqlStr=[" + $scope.mySqlStr + "]");
+	$scope.mySqlStr="select user_id, username from sys.all_users order by user_id";
+	$log.info("$scope.mySqlStr=[" + $scope.mySqlStr + "]");
 	
 	$scope.dbQuerySubmit = function() {
 		$http.get("../dbAccess/get_oracle_data.jsp?sqlStr="+ $scope.mySqlStr).success(function(response) {
